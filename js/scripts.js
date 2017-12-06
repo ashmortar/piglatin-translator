0bc49a99e70000256d2c2f4235b77207d72bf2d7
Aaron Ross and Danyel Murray <danyel@>
2017-12-06 15:19:48 -0800

    fix issue with looping due to using the same array and added in break funcitonality to stop loop if a condition is met

--- js/scripts.js
+++ js/scripts.js
@@ -16,22 +16,28 @@ var pigLatin = function(string) {
   var arrayTwo = [];
   for (var j = 0; j < array.length; j +=1)  {
     for (var i = 0; i < array[j].length; i +=1) {
-      debugger;
-      if ((array[j].charAt(i).match(/[u]/i)) && (array[j].charAt(i - 1).match(/[q]/i))){
+      if (array[j].charAt(0).match(/[a, e, i, o, u]/i)) {
+          var pigString = array[j] + "way";
+          arrayTwo.push(pigString)
+          break;
+      } else if ((array[j].charAt(i).match(/[u]/i)) && (array[j].charAt(i - 1).match(/[q]/i))){
           var a = array[j].slice(0, i + 1);
           var b = array[j].slice(i + 1, array[j].length);
           var pigString = b + a + "ay";
           arrayTwo.push(pigString);
+          break;
       } else if ((array[j].charAt(0).match(/[^y]/i)) && (array[j].charAt(i).match(/[a, e, i, o, u, y]/i))){
           var a = array[j].slice(0, i);
           var b = array[j].slice(i, array[j].length);
           var pigString = b + a + "ay";
           arrayTwo.push(pigString);
+          break;
       } else if ((array[j].charAt(0).match(/[y]/i)) && (array[j].charAt(i).match(/[a, e, i, o, u,]/i))) {
           var a = array[j].slice(0, i);
           var b = array[j].slice(i, array[j].length);
           var pigString = b + a + "ay";
           arrayTwo.push(pigString);
+          break;
         }
         else {
           console.log("something is broken");
