eef964da7bf07393682e77bb5f470eeacd453099
Aaron Ross and Danyel Murray <danyel@>
2017-12-06 10:44:25 -0800

    refactor into for loop and add functionality to identify words that start with multiple consonants at the beginning

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
+/*else if (string.charAt(0).match(/[a, e, i, o, u]/i)) {
+   string = string + "way";
+   return string;
+} else if (string.charAt(0).match(/[^a, e, i, o, u]/i)) {
+ var stringAdd = string + string.charAt(0) + "ay";
+ var pigString = stringAdd.slice(1, stringAdd.length);
+ return pigString;
+};*/
+
 //back end logic
 var pigLatin = function(string) {
   if (string.match(/[^a-z]/i)) {
     return false;
-  } else if (string.charAt(0).match(/[a, e, i, o, u]/i)) {
-      string = string + "way";
-      return string;
-  } else if (string.charAt(0).match(/[^a, e, i, o, u]/i)) {
-    var stringAdd = string + string.charAt(0) + "ay";
-    var pigString = stringAdd.slice(1, stringAdd.length);
-    return pigString;
   };
+
+  for (var i = 0; i < string.length; i +=1) {
+    if (string.charAt(0).match(/[a, e, i, o, u]/i)) {
+        string = string + "way";
+        return string;
+    } else if (string.charAt(i).match(/[a, e, i, o, u]/i)) {
+      var a = string.slice(0, i);
+      var b = string.slice(i, string.length);
+      var pigString = b + a + "ay";
+      return pigString;
+      }
+    };
 };
-
-
-
-
-
-
 
 
 
