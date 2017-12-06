8c4f3d51360a951873d2b0b798366354bf55a9e3
Aaron Ross and Danyel Murray <danyel@>
2017-12-06 15:11:18 -0800

    add funcitonality to accept non-punctuated sentences

--- js/scripts.js
+++ js/scripts.js
@@ -12,31 +12,34 @@ var pigLatin = function(string) {
   if (string.match(/[^a-z, " "]/i)) {
     return false;
   };
-
-  for (var i = 0; i < string.length; i +=1) {
-    if (string.charAt(0).match(/[a, e, i, o, u]/i)) {
-        string = string + "way";
-        return string;
-    } else if ((string.charAt(i).match(/[u]/i)) && (string.charAt(i - 1).match(/[q]/i))){
-        var a = string.slice(0, i + 1);
-        var b = string.slice(i + 1, string.length);
-        var pigString = b + a + "ay";
-        return pigString;
-    } else if ((string.charAt(0).match(/[^y]/i)) && (string.charAt(i).match(/[a, e, i, o, u, y]/i))){
-        var a = string.slice(0, i);
-        var b = string.slice(i, string.length);
-        var pigString = b + a + "ay";
-        return pigString;
-    } else if ((string.charAt(0).match(/[y]/i)) && (string.charAt(i).match(/[a, e, i, o, u,]/i))) {
-        var a = string.slice(0, i);
-        var b = string.slice(i, string.length);
-        var pigString = b + a + "ay";
-        return pigString;
-      }
-      else {
-        console.log("something is broken");
-      }
-    };
+  var array = string.split(" ");
+  var arrayTwo = [];
+  for (var j = 0; j < array.length; j +=1)  {
+    for (var i = 0; i < array[j].length; i +=1) {
+      debugger;
+      if ((array[j].charAt(i).match(/[u]/i)) && (array[j].charAt(i - 1).match(/[q]/i))){
+          var a = array[j].slice(0, i + 1);
+          var b = array[j].slice(i + 1, array[j].length);
+          var pigString = b + a + "ay";
+          arrayTwo.push(pigString);
+      } else if ((array[j].charAt(0).match(/[^y]/i)) && (array[j].charAt(i).match(/[a, e, i, o, u, y]/i))){
+          var a = array[j].slice(0, i);
+          var b = array[j].slice(i, array[j].length);
+          var pigString = b + a + "ay";
+          arrayTwo.push(pigString);
+      } else if ((array[j].charAt(0).match(/[y]/i)) && (array[j].charAt(i).match(/[a, e, i, o, u,]/i))) {
+          var a = array[j].slice(0, i);
+          var b = array[j].slice(i, array[j].length);
+          var pigString = b + a + "ay";
+          arrayTwo.push(pigString);
+        }
+        else {
+          console.log("something is broken");
+        }
+      };
+    var result = arrayTwo.join(" ");
+  };
+  return result;
 };
 
 
